8c2cb8fad3ce19628558474d83c2dab39a1f7c5d
desoares1975 <desoares@ig.com.br>
2013-11-04 16:26:12 -0200

    cleared folder

--- egghead/main.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('superhero', []);
-
-app.directive('superman', function () {
-  return {
-    restrict: ''E',
-    template: '<div>Aqui estou para salver o dia</div>'
-  };
-});
